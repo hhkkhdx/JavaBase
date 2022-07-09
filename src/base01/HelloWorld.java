378f1b1d3585c25fee99b847563fed42dec6b52d
炫 <1490911923@qq.com>
2022-07-10 05:50:44 +0800

    Test

--- src/base01/HelloWorld.java
+++ src/base01/HelloWorld.java
@@ -1,7 +1,16 @@
 package base01;
 
+/**
+ * @BelongsProject: JavaSE
+ * @BelongsPackage: base01
+ * @Author: xuan
+ * @CreateTime: 2022-07-10  05:23
+ * @Description: 开始入门
+ * @Version: 1.0
+ */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("Hello world!");
     }
+
 }
--- src/base01/TestLiteral.java
+++ src/base01/TestLiteral.java
@@ -1,21 +1,35 @@
 package base01;
 
+/**
+ * @BelongsProject: JavaSE
+ * @BelongsPackage: base01
+ * @Author: xuan
+ * @CreateTime: 2022-07-10  05:23
+ * @Description: 字母量
+ * @Version: 1.0
+ */
 public class TestLiteral {
     public static void main(String[] args) {
-        // 1.整数
+        // 1. 整数
         System.out.println(666);
 
-        // 2.小数
+        // 2. 小数
         System.out.println(99.5);
 
-
-        System.out.println('b'); // 3.字符: 必须用单引号引起来，且有且只有一个字符
+        // 3. 字符: 必须用单引号引起来，且有且只有一个字符
+        System.out.println('b');
         System.out.println('a');
         System.out.println(' '); // 空字符
 //        System.out.println(''); // 这种写法错误
-
-        // 4.特殊字符:  \n 换行， \t 代表一个tab
+        // 特殊字符:  \n 换行， \t 代表一个tab
         System.out.println("11\n2");
         System.out.println("11\t2");
+
+        // 4. 字符串: 必须用双引号引起来，里面内容随意
+        System.out.println("hello");
+
+        // 5. 布尔值
+        System.out.println(true);
+        System.out.println(false);
     }
 }
